4b9a033fd96fe2462b0d1c068668a747e58a6196
Rokas Meškauskas <153185726+RokasMesk@users.noreply.github.com>
2024-01-22 22:34:33 +0300

    init commit

---
+++ CodePulseAPI/CodePulseAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodePulseAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CodePulseAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
