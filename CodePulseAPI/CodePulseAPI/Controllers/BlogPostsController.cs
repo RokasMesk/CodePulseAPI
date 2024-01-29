30af5ee47630043dbac33673aceba9f57bcff64d
Rokas Meškauskas <153185726+RokasMesk@users.noreply.github.com>
2024-01-29 23:44:13 +0300

    added blogPost controller, created seperate repository for it, added some crud methods

---
+++ CodePulseAPI/CodePulseAPI/Controllers/BlogPostsController.cs
@@ -0,0 +1,74 @@
+﻿using CodePulseAPI.Models.Domain;
+using CodePulseAPI.Models.DTO;
+using CodePulseAPI.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository _blogPostRepostiory;
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepostiory = blogPostRepository;
+        }
+        // POST: {apibaseurl}/api/blogposts
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            var blogPost = new BlogPost
+            {
+                Title = request.Title,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                PublishedDate = request.PublishedDate,
+                ShortDescription = request.ShortDescription,
+                Author = request.Author,
+                UrlHandle = request.UrlHandle
+            };
+            await _blogPostRepostiory.CreateAsync(blogPost);
+            // Convert Domain model back to dto
+            var response = new BlogPostDTO
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                PublishedDate = blogPost.PublishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                IsVisible = blogPost.IsVisible,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle
+            };
+            return Ok(response);
+        }
+        // GET: {apibaseurl}/api/blogposts
+        [HttpGet]
+        public async  Task<IActionResult> GetAllBlogPosts()
+        {
+            var blogPosts = await _blogPostRepostiory.GetAllAsync();
+            // convert domain model to DTO
+            var response = new List<BlogPostDTO>();
+            foreach (var blogPost in blogPosts)
+            {
+                response.Add(new BlogPostDTO
+                {
+                    Id = blogPost.Id,
+                    Author = blogPost.Author,
+                    Content = blogPost.Content,
+                    PublishedDate = blogPost.PublishedDate,
+                    ShortDescription = blogPost.ShortDescription,
+                    IsVisible = blogPost.IsVisible,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    Title = blogPost.Title,
+                    UrlHandle = blogPost.UrlHandle
+                });
+            }
+            return Ok(response);
+        }
+    }
+}
---
+++ CodePulseAPI/CodePulseAPI/Repositories/Implementation/BlogPostRepository.cs
@@ -0,0 +1,27 @@
+﻿using CodePulseAPI.Data;
+using CodePulseAPI.Models.Domain;
+using CodePulseAPI.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulseAPI.Repositories.Implementation
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public BlogPostRepository(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public async Task<BlogPost> CreateAsync(BlogPost blogPost)
+        {
+            await _db.BlogPosts.AddAsync(blogPost);
+            await _db.SaveChangesAsync();
+            return blogPost;
+        }
+
+        public async Task<IEnumerable<BlogPost>> GetAllAsync()
+        {
+            return await _db.BlogPosts.ToListAsync();
+        }
+    }
+}
